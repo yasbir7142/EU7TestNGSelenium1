938b4758441ceb95251ee8506c614130ffb309c9
yasbir7142 <yasbiryavuz7142@gmail.com>
2021-12-23 21:44:20 +0100

    Update Text1.java

--- src/test/java/com/cybertek/tests/HomeWorkAutomation/Text1.java
+++ src/test/java/com/cybertek/tests/HomeWorkAutomation/Text1.java
@@ -9,10 +9,13 @@ public class Text1 {
 
     public static void main(String[] args) {
 
+        //open the browser
         WebDriver driver = WebDriverFactory.getDriver("chrome");
+
         driver.get("https://www.ebay.de/");
         driver.manage().window().maximize();
 
+        //find the search button
         WebElement SearchItem = driver.findElement(By.id("gh-ac"));
         SearchItem.sendKeys("laptop");
     }
